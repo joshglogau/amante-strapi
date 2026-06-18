a67f02920b105c372035e46eabfdd8f15e55bc33
joshglogau <josh.glogau@siliconswamp.io>
2026-06-18 14:30:46 -0400

    Clean stale API directories before deploy

---
+++ scripts/clean-stale-api-dirs.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+const staleApiDirs = ['about', 'article', 'global', 'blog'];
+const apiRoot = path.join(__dirname, '..', 'src', 'api');
+
+for (const dir of staleApiDirs) {
+  fs.rmSync(path.join(apiRoot, dir), { recursive: true, force: true });
+}
